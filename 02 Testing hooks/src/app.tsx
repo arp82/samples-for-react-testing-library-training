554fb1163fdfa49e43178779174d12ce35238774
Daily <daily.miranda@blue-tc.com>
2020-04-01 10:27:03 +0200

    Preparing repo

--- 02 Testing hooks/src/app.tsx
+++ 02 Testing hooks/src/app.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { MyComponent } from './myComponent';
+import { MyFruits } from './myComponent/myFruits';
 
 export const App: React.FunctionComponent = props => (
   <div>
     <MyComponent nameFromProps="Fruit User" />
+    <MyFruits/>
   </div>
 );
